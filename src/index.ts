353b103382d554e61f150024e844a0641aeb614b
rameshkasula <rameshlearningadvance@gmail.com>
2025-06-01 01:12:52 +0530

    Refactor project configuration: update package.json scripts for development and production, enhance tsconfig.json settings for better type checking and output, and improve error handling in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,11 +16,24 @@ const PORT = ENV_VARIABLES.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
+
 app.use(logRequest); // Log all requests
+
+app.use("*", (req, res) => {
+    res.status(404).json({
+        message: 'Route Not Found',
+        success: false,
+        data: []
+    });
+});
 
 // Example route
 app.get('/', (req, res) => {
-    res.send('Energy Analysis API is running');
+    res.status(200).json({
+        message: 'Energy Analysis API is running',
+        success: true,
+        data: []
+    });
 });
 
 const startServer = async () => {
